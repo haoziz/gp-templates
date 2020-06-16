3a7eab4449c965c3a9b55dc0de2afd865e6d258e
sunxiyuan <sxy@ibadboy.net>
2020-06-16 15:04:09 +0800

    更新：页头和页尾模板使用网站主题样式

--- header.php
+++ header.php
@@ -10,11 +10,23 @@
 
 	gp_head();
 	?>
+	<?php
+	/**
+	 * Hook - zakra_action_head
+	 *
+	 * Functions hooked into zakra_action_head action
+	 *
+	 * @hooked zakra_head - 10
+	 */
+	do_action( 'zakra_action_head' );
+	?>
+
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class( 'no-js' ); ?>>
 	<script type="text/javascript">document.body.className = document.body.className.replace('no-js','js');</script>
-
+<!--
 	<header class="gp-bar clearfix">
 		<h1>
 			<a href="<?php echo esc_url( gp_url( '/' ) ); ?>" rel="home">
@@ -40,8 +52,89 @@
 			<?php echo gp_nav_menu( 'side' ); ?>
 		</nav>
 	</header>
+-->
 
-	<div class="gp-content">
+	<?php
+	/**
+	 * WordPress function to load custom scripts after body.
+	 *
+	 * Introduced in WordPress 5.2.0
+	 *
+	 * @since Zakra 1.2.3
+	 */
+	if ( function_exists( 'wp_body_open' ) ) {
+		wp_body_open();
+	}
+	?>
+
+	<?php
+	/**
+	 * Hook - zakra_action_before
+	 *
+	 * @hooked zakra_page_start - 10
+	 * @hooked zakra_skip_to_content - 15
+	 */
+	do_action( 'zakra_action_before' );
+	?>
+
+	<?php
+	/**
+	 * Hook - zakra_action_before_header
+	 *
+	 * @hooked zakra_header_start - 10
+	 */
+	do_action( 'zakra_action_before_header' );
+	?>
+
+	<?php
+	/**
+	 * Hook - zakra_action_header_top
+	 *
+	 * @hooked zakra_header_top - 10
+	 */
+	do_action( 'zakra_action_header_top' );
+	?>
+
+	<?php
+	/**
+	 * Hook - zakra_action_before_header_main
+	 *
+	 * @hooked zakra_before_header_main - 10
+	 */
+	do_action( 'zakra_action_before_header_main' );
+	?>
+
+	<?php
+	/**
+	 * Hook - zakra_action_header_main
+	 *
+	 * @hooked zakra_header_main_site_branding - 10
+	 * @hooked zakra_header_main_site_navigation - 15
+	 * @hooked zakra_header_main_action- 20
+	 */
+	do_action( 'zakra_action_header_main' );
+	?>
+
+	<?php
+	/**
+	 * Hook - zakra_action_after_header_main
+	 *
+	 * @hooked zakra_header - 10
+	 */
+	do_action( 'zakra_action_after_header_main' );
+	?>
+
+	<?php
+	/**
+	 * Hook - zakra_action_after_header
+	 *
+	 * @hooked zakra_header_end - 10
+	 */
+	do_action( 'zakra_action_after_header' );
+	?>
+
+
+	<div class="gp-content" style="width: 1160px; margin-left: auto; margin-right: auto;">
 		<?php echo gp_breadcrumb(); ?>
 
 		<div id="gp-js-message" class="gp-js-message"></div>
